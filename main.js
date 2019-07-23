57fd022789774621865fb1e55eba71f703d2fd88
egor <eegor650@gmail.com>
2019-07-23 11:54:37 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => mainWindow = null)
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
